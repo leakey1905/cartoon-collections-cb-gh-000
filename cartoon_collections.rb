dbcb7963426ca1bd5fa5edd305bdb3db49de381c
Ji Li <liji15@mails.tsinghua.edu.cn>
2018-11-23 07:09:23 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,21 +1,9 @@
-def roll_call_dwarves(dwarves)
-  number = dwarves.length
-  i = 0
-  dwarves.each |dwarve| do
-    puts "#{i + 1}. #{dwarve}"
-    i += 1
-  end
-end
-
-def summon_captain_planet# code an argument here
-  # Your code here
-end
-
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
-
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
-end
+dwarves = %w[
+  Doc
+  Dopey
+  Bashful
+  Grumpy
+  Sneezy
+  Sleepy
+  Happy
+]
